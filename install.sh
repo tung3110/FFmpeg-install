13b6b698024873dff6098934ee9f065fd6e20cff
tung3110 <tungnh@outlook.com>
2024-09-07 10:19:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,12 @@ cp libffmpeg/libx264.so.155  /usr/lib/x86_64-linux-gnu/libx264.so.155
 
 cp libffmpeg/libx264.so  /usr/lib/x86_64-linux-gnu/libx264.so.155
 
+cp libffmpeg/libsrt.so  /usr/lib/x86_64-linux-gnu/libsrt.so
+
+cp libffmpeg/libsrt.so.1  /usr/lib/x86_64-linux-gnu/libsrt.so.1
+
+cp libffmpeg/libsrt.so.1.4.0  /usr/lib/x86_64-linux-gnu/libsrt.so.1.4.0
+
 cp ffmpeg /usr/bin
 
 cp ffplay /usr/bin
